42459c40582e2d4373e72bce0576bed194c4cd6a
ZeroOrInfinity <zyw23zyw23@163.com>
2020-09-24 23:45:45 +0800

    添加 signUpUrl permitAll 权限.

--- social/src/main/java/top/dcenter/ums/security/social/config/SocialSecurityAutoConfigurerAware.java
+++ social/src/main/java/top/dcenter/ums/security/social/config/SocialSecurityAutoConfigurerAware.java
@@ -51,6 +51,7 @@ public class SocialSecurityAutoConfigurerAware implements HttpSecurityAware {
         permitAllMap.put(socialProperties.getCallbackUrl(), null);
         permitAllMap.put(socialProperties.getCallbackUrl() + "/*", null);
         permitAllMap.put(socialProperties.getSocialUserRegisterUrl(), null);
+        permitAllMap.put(socialProperties.getSignUpUrl(), null);
 
         Map<String, Map<String, Set<String>>> resultMap = new HashMap<>(1);
 
