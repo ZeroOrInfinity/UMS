9f61b92316704f634d9013946bccd6464c9c89b9
ZeroOrInfinity <zyw23zyw23@163.com>
2020-11-08 22:52:34 +0800

    改进: 添加 多租户 与 SCOPE 的角色资源服务接口

--- core/src/main/java/top/dcenter/ums/security/core/api/permission/service/RolePermissionsService.java
+++ core/src/main/java/top/dcenter/ums/security/core/api/permission/service/RolePermissionsService.java
@@ -34,17 +34,25 @@ import java.util.List;
  * 注意: <br>
  * 1. 另外在添加资源时, 通过{@link PermissionType#getPermission()} 来规范的权限格式, 因为要支持 restful 风格的 Api,
  * 在授权时需要对 {@link HttpMethod} 与对应的权限进行匹配判断<br>
- * 2. 修改或添加角色资源一定要更新 {@link UriAuthorizeService#updateRolesAuthorities()} 缓存, 有两种方式: 一种发布事件, 另一种是直接调用服务;<br>
+ * 2. 修改或添加角色资源时一定要调用 {@link UpdateAuthoritiesService} 对应的方法, 有两种方式: 一种发布事件, 另一种是直接调用对应服务;<br>
  * <pre>
  *     // 1. 推荐用发布事件(异步执行)
  *     applicationContext.publishEvent(new UpdateRolesAuthoritiesEvent(true));
  *     // 2. 直接调用服务
- *     abstractUriAuthorizeService.updateRolesAuthorities();
- *     // 3. 实现此接口 {@link #updateResourcesOfRole(Long, Long...)} 方法不需要执行上两种方法的操作, 已通过 AOP 实现发布 UpdateRolesAuthoritiesEvent 事件.
+ *     // 基于角色
+ *     UpdateAuthoritiesService.updateAuthoritiesOfAllRoles();
+ *     // 基于多租户
+ *     UpdateAuthoritiesService.updateAuthoritiesOfAllTenant();
+ *     // 基于 SCOPE
+ *     UpdateAuthoritiesService.updateAuthoritiesOfAllScopes();
+ *     // 3. 实现此接口 {@link #updateResourcesOfRole(Long, Long...)}, {@link #updateResourcesOfTenant(String, Long, Long...)},
+ *     {@link #updateResourcesOfScope(String, Long, Long...)} 的方法不需要执行上两种方法的操作,
+ *     已通过 AOP 实现发布 UpdateRolesAuthoritiesEvent 事件.
  * </pre>
  * @author YongWu zheng
  * @version V2.0  Created by 2020/11/6 23:33
  */
+@SuppressWarnings("unused")
 public interface RolePermissionsService<T> {
 
     /**
@@ -56,21 +64,36 @@ public interface RolePermissionsService<T> {
      * @return  是否操作成功
      * @throws RolePermissionsException 更新角色资源信息失败
      */
-    boolean updateResourcesOfRole(Long roleId, Long... resourceIds) throws RolePermissionsException;
+    default boolean updateResourcesOfRole(Long roleId, Long... resourceIds) throws RolePermissionsException {
+        throw new RuntimeException("未实现 更新角色(roleId)所拥有的资源信息的接口逻辑");
+    }
+    /**
+     * 更新多租户的角色(roleId)所拥有的资源信息.
+     * 注意: 此方法既有新增逻辑, 也有更新逻辑, 还有删除逻辑; 逻辑比较复杂, 通用做法是先根据角色 Id 删除角色所拥有的资源, 再根据(resourceIds)新增资源.
+     * 建议角色资源关系表的主键为(roleId, resourceId)的组合主键, 不设置自增主键.
+     * @param tenantAuthority   多租户权限, 如: TENANT_110110, 110110 为租户 ID
+     * @param roleId            角色 Id
+     * @param resourceIds       资源 Ids
+     * @return  是否操作成功
+     * @throws RolePermissionsException 更新角色资源信息失败
+     */
+    default boolean updateResourcesOfTenant(String tenantAuthority, Long roleId, Long... resourceIds) throws RolePermissionsException {
+        throw new RuntimeException("未实现 更新 多租户 的角色(roleId)所拥有的资源信息的接口逻辑");
+    }
 
     /**
-     * 获取 角色 ID 所有的资源(Resources)信息, 注意实现此方法时注意在逻辑里鉴权, 即只能获取本角色资源的信息列表,
-     * 如有角色继承关系{@link RoleHierarchy}, 还可以获取继承角色的角色信息. 例如:
-     * <pre>
-     * 角色继承: ROLE_A &gt; ROLE_B &gt; ROLE_C.
-     * 那么角色: ROLE_B.
-     * 可以获取: ROLE_B, ROLE_C 角色信息.
-     * </pre>
-     * @param roleId  角色 Id
-     * @return  返回资源信息列表
-     * @throws RolePermissionsException 查询角色资源信息失败
+     * 更新 scope 的角色(roleId)所拥有的资源信息.
+     * 注意: 此方法既有新增逻辑, 也有更新逻辑, 还有删除逻辑; 逻辑比较复杂, 通用做法是先根据角色 Id 删除角色所拥有的资源, 再根据(resourceIds)新增资源.
+     * 建议角色资源关系表的主键为(roleId, resourceId)的组合主键, 不设置自增主键.
+     * @param scopeAuthority    scope 权限, 如: SCOPE_read
+     * @param roleId            角色 Id
+     * @param resourceIds       资源 Ids
+     * @return  是否操作成功
+     * @throws RolePermissionsException 更新角色资源信息失败
      */
-    List<T> findAllResourcesByRoleId(Long roleId) throws RolePermissionsException;
+    default boolean updateResourcesOfScope(String scopeAuthority, Long roleId, Long... resourceIds) throws RolePermissionsException {
+        throw new RuntimeException("未实现 更新 scope 的角色(roleId)所拥有的资源信息的接口逻辑");
+    }
 
     /**
      * 根据 角色 获取所有的资源(Resources)信息, 注意实现此方法时注意在逻辑里鉴权, 即只能获取本角色资源的信息列表,
@@ -84,6 +107,42 @@ public interface RolePermissionsService<T> {
      * @return      返回资源信息列表
      * @throws RolePermissionsException 查询角色资源信息失败
      */
-    List<T> findAllResourcesByRole(String role) throws RolePermissionsException;
+    default List<T> findAllResourcesByRole(String role) throws RolePermissionsException {
+        throw new RuntimeException("未实现根据 角色 获取所有的资源(Resources)信息的接口逻辑");
+    }
+
+    /**
+     * 根据 租户权限 获取所有的资源(Resources)信息, 注意实现此方法时注意在逻辑里鉴权, 即只能获取本角色资源的信息列表,
+     * 如有角色继承关系{@link RoleHierarchy}, 还可以获取继承角色的角色信息. 例如:
+     * <pre>
+     * 角色继承: ROLE_A &gt; ROLE_B &gt; ROLE_C.
+     * 那么角色: ROLE_B.
+     * 可以获取: ROLE_B, ROLE_C 角色信息.
+     * </pre>
+     * @param tenantAuthority   多租户权限, 如: TENANT_110110, 110110 为租户 ID
+     * @param role              角色, 例如: ROLE_A
+     * @return                  返回资源信息列表
+     * @throws RolePermissionsException 查询角色资源信息失败
+     */
+    default List<T> findAllResourcesByTenant(String tenantAuthority, String role) throws RolePermissionsException {
+        throw new RuntimeException("未实现根据 租户Id 获取所有的资源(Resources)信息的接口逻辑");
+    }
+
+    /**
+     * 根据 scope 获取所有的资源(Resources)信息, 注意实现此方法时注意在逻辑里鉴权, 即只能获取本角色资源的信息列表,
+     * 如有角色继承关系{@link RoleHierarchy}, 还可以获取继承角色的角色信息. 例如:
+     * <pre>
+     * 角色继承: ROLE_A &gt; ROLE_B &gt; ROLE_C.
+     * 那么角色: ROLE_B.
+     * 可以获取: ROLE_B, ROLE_C 角色信息.
+     * </pre>
+     * @param scopeAuthority    scope 权限, 如: SCOPE_read
+     * @param role              角色, 例如: ROLE_A
+     * @return                  返回资源信息列表
+     * @throws RolePermissionsException 查询角色资源信息失败
+     */
+    default List<T> findAllResourcesByScope(String scopeAuthority, String role) throws RolePermissionsException {
+        throw new RuntimeException("未实现根据 scope 获取所有的资源(Resources)信息的接口逻辑");
+    }
 
 }
