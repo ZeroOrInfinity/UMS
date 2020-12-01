32a929d3a3c51f4db25853fe5afb09104fa3e17b
ZeroOrInfinity <zyw23zyw23@163.com>
2020-12-01 16:49:19 +0800

    改进: 添加默认方法, 已登录情况下获取租户 ID 的方法 getTenantId(Authentication).

--- core/src/main/java/top/dcenter/ums/security/core/api/tenant/handler/TenantContextHolder.java
+++ core/src/main/java/top/dcenter/ums/security/core/api/tenant/handler/TenantContextHolder.java
@@ -25,17 +25,23 @@ package top.dcenter.ums.security.core.api.tenant.handler;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserCache;
+import top.dcenter.ums.security.common.enums.ErrorCodeEnum;
 import top.dcenter.ums.security.core.api.service.UmsUserDetailsService;
 import top.dcenter.ums.security.core.exception.TenantIdNotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+
+import static top.dcenter.ums.security.core.api.permission.service.AbstractUriAuthorizeService.DEFAULT_TENANT_PREFIX;
 
 /**
  * 多租户处理器. 实现此接口并注入 IOC 容器后, 会自动注入 UMS 默认实现的注册/登录/授权组件, 要实现 ums 框架具有多租户功能, 必须实现此接口并注入 IOC 容器.<br>
  * 功能: <br>
  * 1. {@link #tenantIdHandle(HttpServletRequest, String)}从注册用户入口或登录用户入口提取 tenantId 及进行必要的逻辑处理(如: tenantId 存入 ThreadLocal, 或存入 session, 或存入 redis 缓存等).<br>
- * 2. {@link #getTenantId()}方便后续用户注册、登录、授权的数据处理(如：sql 添加 tenantId 的条件，注册用户添加 TENANT_tenantId 权限，根据 tenantId 获取角色的权限数据).<br>
+ * 2. {@link #getTenantId()},方便后续用户注册、登录、授权的数据处理(如：sql 添加 tenantId 的条件，注册用户添加 TENANT_tenantId 权限，根据 tenantId 获取角色的权限数据).<br>
+ * 3. {@link #getTenantId(Authentication)} 默认实现方法, 用户已登录的情况下, 获取租户 ID, 直接从 {@code authority} 中解析获取.<br>
  *
  * 注意:<br>
  * 1. 多租户系统中, 在未登录时需要用到 tenantId 的接口, 如: {@link UserCache#getUserFromCache(String)}/{@link UmsUserDetailsService} 等接口,
@@ -64,10 +70,53 @@ public interface TenantContextHolder {
     String tenantIdHandle(@NonNull HttpServletRequest request, @Nullable String tenantId) throws TenantIdNotFoundException;
 
     /**
-     * 获取租户 ID(如: 从 ThreadLocal 中获取 tenantId, 或从 session 中获取 tenantId, 或从 redis 缓存中获取 tenantId等)
+     * 获取租户 ID:
+     * 1. 未登录情况从缓存获取租户 ID(如: 从 ThreadLocal 中获取 tenantId, 或从 session 中获取 tenantId, 或从 redis 缓存中获取 tenantId等).<br>
+     * 2. 已登录情况可以调用默认方法 {@link #getTenantId(Authentication)}
+     * <pre>
+     * Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+     * if (authentication.isAuthenticated()) {
+     *     // 已登录用户获取租户 id
+     *     return getTenantId(authentication);
+     * }
+     * </pre>
      * @return 返回 tenantId
      * @throws TenantIdNotFoundException    获取不到租户 ID 异常
      */
     @NonNull
     String getTenantId() throws TenantIdNotFoundException;
+
+    /**
+     * 用户已登录的情况下, 获取租户 ID, 直接从 {@code authority} 中解析获取.
+     * @param authentication        {@link Authentication} 必须是登录成功的用户, 未登录用户直接抛 {@link TenantIdNotFoundException} 异常.
+     * @return  返回租户 id
+     * @throws TenantIdNotFoundException    获取不到租户 ID 异常
+     */
+    @NonNull
+    default String getTenantId(@NonNull Authentication authentication) throws TenantIdNotFoundException {
+
+        if (!authentication.isAuthenticated()) {
+            // 用户未登录的情况
+            throw new TenantIdNotFoundException(ErrorCodeEnum.TENANT_ID_NOT_FOUND, null, authentication.getName());
+        }
+
+        // 用户已登录的情况
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String tenantId = null;
+        String auth;
+        for (GrantedAuthority authority : authorities) {
+            auth = authority.getAuthority();
+            if (auth.startsWith(DEFAULT_TENANT_PREFIX)) {
+                tenantId = auth.substring(DEFAULT_TENANT_PREFIX.length());
+                break;
+            }
+        }
+
+        if (null == tenantId) {
+            throw new TenantIdNotFoundException(ErrorCodeEnum.TENANT_ID_NOT_FOUND, null, authentication.getName());
+        }
+
+        return tenantId;
+    }
 }
