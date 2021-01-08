e2fc3c91432ebcf48868d9ad302e82b81b587a63
ZeroOrInfinity <zyw23zyw23@163.com>
2021-01-08 13:29:08 +0800

    改进: 添加 UmsBearerTokenResolver 忽略解析jwt 的 urls 字段 ignoreUrls 及相关处理.

--- jwt/src/main/java/top/dcenter/ums/security/jwt/resolver/UmsBearerTokenResolver.java
+++ jwt/src/main/java/top/dcenter/ums/security/jwt/resolver/UmsBearerTokenResolver.java
@@ -23,136 +23,154 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.server.resource.BearerTokenError;
 import org.springframework.security.oauth2.server.resource.BearerTokenErrors;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import top.dcenter.ums.security.common.utils.UrlUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static java.util.Objects.nonNull;
 
 /**
  * The default {@link BearerTokenResolver} implementation based on RFC 6750.
  *
  * @author Vedran Pavic
  * @author YongWu zheng
- * @since 5.1
  * @see <a href="https://tools.ietf.org/html/rfc6750#section-2" target="_blank">RFC 6750
  * Section 2: Authenticated Requests</a>
+ * @since 5.1
  */
 public final class UmsBearerTokenResolver implements BearerTokenResolver {
 
-	private static final Pattern AUTHORIZATION_PATTERN = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+=*)$",
-	                                                                     Pattern.CASE_INSENSITIVE);
+    private static final Pattern AUTHORIZATION_PATTERN = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+=*)$",
+                                                                         Pattern.CASE_INSENSITIVE);
 
-	private final String jwtByRefreshTokenUri;
+    private final String requestParameterName;
 
-	private final String requestParameterName;
+    private boolean allowFormEncodedBodyParameter = false;
 
-	private boolean allowFormEncodedBodyParameter = false;
+    private boolean allowUriQueryParameter = false;
 
-	private boolean allowUriQueryParameter = false;
+    private String bearerTokenHeaderName = HttpHeaders.AUTHORIZATION;
 
-	private String bearerTokenHeaderName = HttpHeaders.AUTHORIZATION;
+    /**
+     * 需要忽略的 url, 如: 通过 refreshToken 刷新 jwt uri, 登录的 uri. 支持 {@link AntPathMatcher} 模式.
+     */
+    private final Set<String> ignoreUrls = new HashSet<>();
 
+    private final AntPathMatcher matcher = new AntPathMatcher();
 
-	public UmsBearerTokenResolver(@NonNull String requestParameterName, @Nullable String jwtByRefreshTokenUri) {
-		this.requestParameterName = requestParameterName;
-		this.jwtByRefreshTokenUri = jwtByRefreshTokenUri;
-	}
+    public UmsBearerTokenResolver(@NonNull String requestParameterName, @Nullable String jwtByRefreshTokenUri) {
+        this.requestParameterName = requestParameterName;
+        this.ignoreUrls.add(jwtByRefreshTokenUri);
+    }
 
+    @Override
+    public String resolve(HttpServletRequest request) {
+        final String pathWithinApplication = UrlUtil.getUrlPathHelper().getPathWithinApplication(request);
+        boolean anyMatch = ignoreUrls.stream()
+                                     .anyMatch((ignoreUrl) -> matcher.match(ignoreUrl,
+                                                                            pathWithinApplication));
 
-	@Override
-	public String resolve(HttpServletRequest request) {
-		if (nonNull(jwtByRefreshTokenUri)) {
-			// 如果是刷新 jwt 的 uri 则返回 null
-			boolean isRefreshJwtUri = jwtByRefreshTokenUri.equals(UrlUtil.getUrlPathHelper().getPathWithinApplication(request));
-			if (isRefreshJwtUri) {
-				return null;
-			}
-		}
+        // 如果是匹配 ignoreUrls 则返回 null
+        if (anyMatch) {
+            return null;
+        }
 
-		String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
-		String parameterToken = resolveFromRequestParameters(request);
-		if (authorizationHeaderToken != null) {
-			if (parameterToken != null) {
-				BearerTokenError error = BearerTokenErrors
-						.invalidRequest("Found multiple bearer tokens in the request");
-				throw new OAuth2AuthenticationException(error);
-			}
-			return authorizationHeaderToken;
-		}
-		if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
-			return parameterToken;
-		}
-		return null;
-	}
+        String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
+        String parameterToken = resolveFromRequestParameters(request);
+        if (authorizationHeaderToken != null) {
+            if (parameterToken != null) {
+                BearerTokenError error = BearerTokenErrors
+                        .invalidRequest("Found multiple bearer tokens in the request");
+                throw new OAuth2AuthenticationException(error);
+            }
+            return authorizationHeaderToken;
+        }
+        if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
+            return parameterToken;
+        }
+        return null;
+    }
 
-	/**
-	 * Set if transport of access token using form-encoded body parameter is supported.
-	 * Defaults to {@code false}.
-	 * @param allowFormEncodedBodyParameter if the form-encoded body parameter is
-	 * supported
-	 */
-	public void setAllowFormEncodedBodyParameter(boolean allowFormEncodedBodyParameter) {
-		this.allowFormEncodedBodyParameter = allowFormEncodedBodyParameter;
-	}
+    /**
+     * 添加需要忽略解析 JWT token 的 url
+     *
+     * @param ignoreUrls 需要忽略解析 JWT token 的 urls, 支持 {@link AntPathMatcher} 模式.
+     */
+    public void addIgnoreUrls(Set<String> ignoreUrls) {
+        this.ignoreUrls.addAll(ignoreUrls);
+    }
 
-	/**
-	 * Set if transport of access token using URI query parameter is supported. Defaults
-	 * to {@code false}.
-	 *
-	 * The spec recommends against using this mechanism for sending bearer tokens, and
-	 * even goes as far as stating that it was only included for completeness.
-	 * @param allowUriQueryParameter if the URI query parameter is supported
-	 */
-	public void setAllowUriQueryParameter(boolean allowUriQueryParameter) {
-		this.allowUriQueryParameter = allowUriQueryParameter;
-	}
+    /**
+     * Set if transport of access token using form-encoded body parameter is supported.
+     * Defaults to {@code false}.
+     *
+     * @param allowFormEncodedBodyParameter if the form-encoded body parameter is
+     *                                      supported
+     */
+    public void setAllowFormEncodedBodyParameter(boolean allowFormEncodedBodyParameter) {
+        this.allowFormEncodedBodyParameter = allowFormEncodedBodyParameter;
+    }
 
-	/**
-	 * Set this value to configure what header is checked when resolving a Bearer Token.
-	 * This value is defaulted to {@link HttpHeaders#AUTHORIZATION}.
-	 *
-	 * This allows other headers to be used as the Bearer Token source such as
-	 * {@link HttpHeaders#PROXY_AUTHORIZATION}
-	 * @param bearerTokenHeaderName the header to check when retrieving the Bearer Token.
-	 * @since 5.4
-	 */
-	public void setBearerTokenHeaderName(String bearerTokenHeaderName) {
-		this.bearerTokenHeaderName = bearerTokenHeaderName;
-	}
+    /**
+     * Set if transport of access token using URI query parameter is supported. Defaults
+     * to {@code false}.
+     * <p>
+     * The spec recommends against using this mechanism for sending bearer tokens, and
+     * even goes as far as stating that it was only included for completeness.
+     *
+     * @param allowUriQueryParameter if the URI query parameter is supported
+     */
+    public void setAllowUriQueryParameter(boolean allowUriQueryParameter) {
+        this.allowUriQueryParameter = allowUriQueryParameter;
+    }
 
-	private String resolveFromAuthorizationHeader(HttpServletRequest request) {
-		String authorization = request.getHeader(this.bearerTokenHeaderName);
-		String bearer = "bearer";
-		if (!StringUtils.startsWithIgnoreCase(authorization, bearer)) {
-			return null;
-		}
-		Matcher matcher = AUTHORIZATION_PATTERN.matcher(authorization);
-		if (!matcher.matches()) {
-			BearerTokenError error = BearerTokenErrors.invalidToken("Bearer token is malformed");
-			throw new OAuth2AuthenticationException(error);
-		}
-		return matcher.group("token");
-	}
+    /**
+     * Set this value to configure what header is checked when resolving a Bearer Token.
+     * This value is defaulted to {@link HttpHeaders#AUTHORIZATION}.
+     * <p>
+     * This allows other headers to be used as the Bearer Token source such as
+     * {@link HttpHeaders#PROXY_AUTHORIZATION}
+     *
+     * @param bearerTokenHeaderName the header to check when retrieving the Bearer Token.
+     * @since 5.4
+     */
+    public void setBearerTokenHeaderName(String bearerTokenHeaderName) {
+        this.bearerTokenHeaderName = bearerTokenHeaderName;
+    }
 
-	private String resolveFromRequestParameters(HttpServletRequest request) {
-		String[] values = request.getParameterValues(requestParameterName);
-		if (values == null || values.length == 0) {
-			return null;
-		}
-		if (values.length == 1) {
-			return values[0];
-		}
-		BearerTokenError error = BearerTokenErrors.invalidRequest("Found multiple bearer tokens in the request");
-		throw new OAuth2AuthenticationException(error);
-	}
+    private String resolveFromAuthorizationHeader(HttpServletRequest request) {
+        String authorization = request.getHeader(this.bearerTokenHeaderName);
+        String bearer = "bearer";
+        if (!StringUtils.startsWithIgnoreCase(authorization, bearer)) {
+            return null;
+        }
+        Matcher matcher = AUTHORIZATION_PATTERN.matcher(authorization);
+        if (!matcher.matches()) {
+            BearerTokenError error = BearerTokenErrors.invalidToken("Bearer token is malformed");
+            throw new OAuth2AuthenticationException(error);
+        }
+        return matcher.group("token");
+    }
 
-	private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
-		return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
-				|| (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
-	}
+    private String resolveFromRequestParameters(HttpServletRequest request) {
+        String[] values = request.getParameterValues(requestParameterName);
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        if (values.length == 1) {
+            return values[0];
+        }
+        BearerTokenError error = BearerTokenErrors.invalidRequest("Found multiple bearer tokens in the request");
+        throw new OAuth2AuthenticationException(error);
+    }
+
+    private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
+        return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
+                || (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
+    }
 
 }
